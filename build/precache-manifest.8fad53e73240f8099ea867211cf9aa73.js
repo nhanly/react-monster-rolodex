1874e0d97c9f56406e48d67d292f229a3c862997
lee <lee@soxes.ch>
2020-06-20 22:38:27 +0700

    adding files for github pages

---
+++ build/precache-manifest.8fad53e73240f8099ea867211cf9aa73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62d4d71d6d1b4233e6799961f3df9fdd",
+    "url": "/react-monster-rolodex/index.html"
+  },
+  {
+    "revision": "ade6d7302fa0a556e71a",
+    "url": "/react-monster-rolodex/static/css/main.397d0874.chunk.css"
+  },
+  {
+    "revision": "b5753794ed0a583090f2",
+    "url": "/react-monster-rolodex/static/js/2.782ae6f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monster-rolodex/static/js/2.782ae6f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ade6d7302fa0a556e71a",
+    "url": "/react-monster-rolodex/static/js/main.9248af8f.chunk.js"
+  },
+  {
+    "revision": "7051277a216650136da5",
+    "url": "/react-monster-rolodex/static/js/runtime-main.f1f63675.js"
+  }
+]);
